c80e3ee4e0875e62ee2bfec6799f13b738acf0ac
Tove582700730 <582700730@qq.com>
2020-07-18 22:45:33 +0800

    商城的第一次提交(导入maven依赖、css、js、layui）

---
+++ src/main/Java/com/tove582700730/service/BookService.java
@@ -0,0 +1,10 @@
+package com.tove582700730.service;
+
+
+
+import java.util.List;
+
+public interface BookService {
+
+
+}
---
+++ src/main/Java/com/tove582700730/service/BookServiceImpl.java
@@ -0,0 +1,16 @@
+package com.tove582700730.service;
+
+import com.tove582700730.dao.BookMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookMapper bookMapper;
+
+
+}
