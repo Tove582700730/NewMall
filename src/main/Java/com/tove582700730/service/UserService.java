4630c16e4bc2125fdaa0564e428add1be1f22a65
Tove582700730 <582700730@qq.com>
2020-07-18 22:48:30 +0800

    商城的第一次提交(导入maven依赖、css、js、layui）

---
+++ src/main/Java/com/tove582700730/service/UserService.java
@@ -0,0 +1,10 @@
+package com.tove582700730.service;
+
+import com.tove582700730.util.ResultVo;
+
+import javax.servlet.http.HttpSession;
+
+public interface UserService {
+
+
+}
---
+++ src/main/Java/com/tove582700730/service/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.tove582700730.service;
+
+import com.tove582700730.dao.UserMapper;
+
+import com.tove582700730.util.Md5Util;
+import com.tove582700730.util.ResultVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+}
