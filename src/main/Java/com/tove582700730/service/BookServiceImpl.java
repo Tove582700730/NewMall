ddb846fffe93500c66695461ca025b1e56be639d
Tove582700730 <582700730@qq.com>
2020-07-21 22:44:20 +0800

    商城的第一次提交(导入maven依赖、css、js、layui）

--- src/main/Java/com/tove582700730/service/BookServiceImpl.java
+++ src/main/Java/com/tove582700730/service/BookServiceImpl.java
@@ -1,6 +1,7 @@
 package com.tove582700730.service;
 
 import com.tove582700730.dao.BookMapper;
+import com.tove582700730.pojo.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +13,14 @@ public class BookServiceImpl implements BookService {
     @Autowired
     private BookMapper bookMapper;
 
+    @Override
+    public List<Book> getBookByTypeId(Integer tid) {
+        List<Book> bookList = bookMapper.getBookByTypeId(tid);
+        return bookList;
+    }
 
+    @Override
+    public Book getBookDetailById(Integer bid) {
+        return bookMapper.selectByPrimaryKey(bid);
+    }
 }
