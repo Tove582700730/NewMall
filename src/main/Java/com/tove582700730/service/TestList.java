2242c9f474647d383407ab9e9a80daf2358681ea
Tove582700730 <582700730@qq.com>
2020-07-25 22:01:08 +0800

    商城的第一次提交(导入maven依赖、css、js、layui）

---
+++ src/main/Java/com/tove582700730/service/TestList.java
@@ -0,0 +1,19 @@
+package com.tove582700730.service;
+
+import com.qianfeng.pojo.Types;
+
+import java.util.ArrayList;
+
+public class TestList {
+    public static void main(String[] args) {
+        Types types = new Types();
+        types.setTname("嘻嘻");
+        ArrayList<Types> list1 = new ArrayList<>();
+        ArrayList<Types> list2 = new ArrayList<>();
+        list1.add(types);
+        list2.add(types);
+        list1.get(0).setTname("哈哈");
+        System.out.println(list1);
+        System.out.println(list2);
+    }
+}
