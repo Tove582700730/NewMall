50cabb662dc3417b01a314891f9c72a9441a05de
Tove582700730 <582700730@qq.com>
2020-07-26 23:31:24 +0800

    商城的第一次提交(导入maven依赖、css、js、layui）

---
+++ src/main/Java/com/tove582700730/service/TypesService.java
@@ -0,0 +1,14 @@
+package com.tove582700730.service;
+
+import com.qianfeng.pojo.Types;
+
+import java.util.List;
+
+public interface TypesService {
+
+    /**
+     * 查询所有菜单集合
+     * @return
+     */
+    List<Types> getTypesList();
+}
