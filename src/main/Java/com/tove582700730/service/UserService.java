6fcff4c3fbf6b74d295df4bdbe3c1634c99895cc
Tove582700730 <582700730@qq.com>
2020-07-27 21:56:23 +0800

    商城的第一次提交(导入maven依赖、css、js、layui）

--- src/main/Java/com/tove582700730/service/UserService.java
+++ src/main/Java/com/tove582700730/service/UserService.java
@@ -1,10 +1,23 @@
 package com.tove582700730.service;
 
+import com.tove582700730.pojo.Users;
 import com.tove582700730.util.ResultVo;
 
 import javax.servlet.http.HttpSession;
 
 public interface UserService {
 
+    /**
+     * 注册
+     * @param users
+     * @return
+     */
+    ResultVo regist(Users users);
 
+    /**
+     * 登录
+     * @param users
+     * @return
+     */
+    ResultVo login(Users users, HttpSession session);
 }
